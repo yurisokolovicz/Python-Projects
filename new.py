7078cda5bf8c73f4a81e7cf78f856ad93e28e536
yurisokolovicz <yurisokolovicz@gmail.com>
2023-01-21 11:57:39 -0300

    Variable Assignments

--- new.py
+++ new.py
@@ -3,12 +3,26 @@
 # print("***********")
 # print (["Yuri", 100, 25.20])
 
-# Numbers
-print(2+1)
-print(2 - 1)
-print(2 * 2)
-print(4 / 2)
-print(7 % 4)
-print(2 ** 2)
+## Numbers
 
-# Variable Assignments
+# print(2+1)
+# print(2 - 1)
+# print(2 * 2)
+# print(4 / 2)
+# print(7 % 4)
+# print(2 ** 2)
+
+## Variable Assignments
+
+a = 5
+b = 1.1
+print(a)
+print(a+a)
+print(type(a))
+print(type(b))
+
+my_income = 1000 # salario
+tax_rate = 0.15 # 15% de impostos 
+may_taxes = my_income * tax_rate
+print(may_taxes)
+
