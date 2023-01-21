3f58b98cba6a3a232669ff051bd59bd9e1154f26
yurisokolovicz <yurisokolovicz@gmail.com>
2023-01-21 11:43:19 -0300

    Numbers

--- new.py
+++ new.py
@@ -1,4 +1,14 @@
-print("***********")
-print("Hello world")
-print("***********")
+# print("***********")
+# print("Hello world")
+# print("***********")
+# print (["Yuri", 100, 25.20])
 
+# Numbers
+print(2+1)
+print(2 - 1)
+print(2 * 2)
+print(4 / 2)
+print(7 % 4)
+print(2 ** 2)
+
+# Variable Assignments
